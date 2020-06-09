a0e501b9039a04becd6eb2ae5513f1ab77ffd147
Ashay <ashaymurceilago@gmail.com>
2020-06-09 12:32:04 +0530

    Add files via upload

---
+++ part_1_Controller-master/src/main/java/com/example/Part1_Api_Service/Part1ApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Part1_Api_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part1ApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part1ApiServiceApplication.class, args);
+	}
+
+}
---
+++ part_1_Controller-master/src/test/java/com/example/Part1_Api_Service/Part1ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Part1_Api_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part1ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
